d76d8b50dc7b0a4746047604b82aea755732b90e
Tom Hummel <tom@interlincx.com>
2012-12-16 15:39:23 -0800

    ignore .fls

--- tnt.d
+++
@@ -1,8 +0,0 @@
-# vim: ft=make
-.PHONY: tnt._graphics
-tnt.aux tnt.aux.make tnt.d tnt.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/base/book.cls)
-tnt.aux tnt.aux.make tnt.d tnt.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/base/makeidx.sty)
-tnt.aux tnt.aux.make tnt.d tnt.pdf: $(call path-norm,/usr/local/texlive/2012/texmf-dist/tex/latex/psnfss/mathptmx.sty)
-tnt.aux tnt.aux.make tnt.d tnt.pdf: $(call path-norm,copy/preface.tex)
-tnt.aux tnt.aux.make tnt.d tnt.pdf: $(call path-norm,tnt.tex)
-.SECONDEXPANSION:
