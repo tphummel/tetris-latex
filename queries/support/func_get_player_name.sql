7fc85f7ff3101da196f5803be2b8357b7533ad6c
Tom Hummel <tom@interlincx.com>
2013-01-06 18:36:45 -0800

    updated func_get_player_name to default to nicknames, and 'Guest' overwriting for random players

--- queries/support/func_get_player_name.sql
+++ queries/support/func_get_player_name.sql
@@ -1,17 +1,20 @@
 DELIMITER $$
 
 DROP FUNCTION IF EXISTS get_player_name$$
-CREATE FUNCTION get_player_name(in_playerid INT, in_type VARCHAR(10))
+CREATE FUNCTION get_player_name(in_playerid INT, in_type VARCHAR(10), in_guest BOOLEAN)
   RETURNS VARCHAR(50)
 BEGIN
   DECLARE v_name VARCHAR(50);
-  IF UPPER(in_type) = 'NICK' THEN
-    SElECT username
+  
+  IF in_guest = '' THEN SET in_guest = true; END IF;
+  
+  IF UPPER(in_type) = 'FULL' THEN
+    SELECT IF(in_guest, IF(username IN ('Tom','JD','Jeran','Dan','Spirk'), CONCAT(firstname, ' ', lastname), 'Guest'), CONCAT(firstname, ' ', lastname))
     INTO v_name
     FROM player
     WHERE playerid = in_playerid;
   ELSE
-    SElECT CONCAT(firstname, ' ', lastname)
+    SELECT IF(in_guest, IF(username IN ('Tom','JD','Jeran','Dan','Spirk'), username, 'Guest'), username)
     INTO v_name
     FROM player
     WHERE playerid = in_playerid;
